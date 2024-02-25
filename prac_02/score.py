a65f972be5d69b35d96f985b7eec99525ae1c59d
LetianYou <160473447+LetianYou@users.noreply.github.com>
2024-02-25 22:33:12 +0800

    final version

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,16 +2,28 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
-# TODO: Fix this!
 
-score = float(input("Enter score: "))
+def main():
+    score = float(input("Enter score: "))
+    result = determine_score(score)
+    print(f"The result is: {result}")
 
-if score <= 0 or score >= 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+    random_score = random.randint(0, 100)
+    random_result = determine_score(random_score)
+    print(f"The random result is: {random_result}")
+
+
+def determine_score(score):
+    if score <= 0 or score >= 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
