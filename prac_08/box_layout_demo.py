c003bf1ab7386f1096a7bdbf863e6d41e1aff23e
LetianYou <160473447+LetianYou@users.noreply.github.com>
2024-03-31 16:48:29 +0800

    attempt 1

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -3,10 +3,22 @@ from kivy.lang import Builder
 
 
 class BoxLayoutDemo(App):
+    """ BoxLayoutDemoApp is a Kivy App for box layout demo """
     def build(self):
+        """ build the Kivy app from the kv file """
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
+
+    def handle_greet(self):
+        """ handle greet, show the format output """
+        print("Test")
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+    def handle_clear(self):
+        """handle clear, clear the input and output"""
+        self.root.ids.input_name.text = ''
+        self.root.ids.output_label.text = ''
 
 
 BoxLayoutDemo().run()
